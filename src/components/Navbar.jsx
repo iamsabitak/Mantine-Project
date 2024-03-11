ecaa4716cf9d645aa88dee7052468fa1be596415
sabita <sabitakhadka091@gmail.com>
2024-03-11 14:06:23 +0545

    created new components home,about,services and contact

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import { NavLink } from "@mantine/core";
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/about">About</NavLink>
+        </li>
+        <li>
+          <NavLink to="/services">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact">Contact</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;

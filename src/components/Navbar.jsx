fa5d290f6d2ae376d2bf50d959a3ba68ea416c97
sabita <sabitakhadka091@gmail.com>
2024-03-11 14:58:56 +0545

    added styled component and one more link of inputexample

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,50 @@
-import { NavLink } from "@mantine/core";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const StyledNav = styled.nav`
+  background-color: #b6bcbc;
+  padding: 5px;
+`;
+
+const StyledUl = styled.ul`
+  display: flex;
+  list-style-type: none;
+  padding: 0;
+`;
+
+const StyledLi = styled.li`
+  margin-right: 10px;
+`;
+
+const StyledLink = styled(Link)`
+  color: black;
+  text-decoration: none;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
 
 function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/about">About</NavLink>
-        </li>
-        <li>
-          <NavLink to="/services">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/contact">Contact</NavLink>
-        </li>
-      </ul>
-    </nav>
+    <StyledNav>
+      <StyledUl>
+        <StyledLi>
+          <StyledLink to="/home">Home</StyledLink>
+        </StyledLi>
+        <StyledLi>
+          <StyledLink to="/about">About</StyledLink>
+        </StyledLi>
+        <StyledLi>
+          <StyledLink to="/services">Services</StyledLink>
+        </StyledLi>
+        <StyledLi>
+          <StyledLink to="/contact">Contact</StyledLink>
+        </StyledLi>
+        <StyledLi>
+          <StyledLink to="/" />
+        </StyledLi>
+      </StyledUl>
+    </StyledNav>
   );
 }
 

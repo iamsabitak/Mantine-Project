c076509c52a0310d9059c346ef4fc37c2b6db8b0
sabita <sabitakhadka091@gmail.com>
2024-03-11 11:14:47 +0545

    implement the inputexample component in app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { MantineProvider } from "@mantine/core";
+import InputExample from "./InputExample";
 
 function App() {
   return (
     <MantineProvider>
       <h1>Vite React App with Mantine</h1>
+      <InputExample />
     </MantineProvider>
   );
 }

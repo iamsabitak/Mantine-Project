a2d654c60a037bff270b91b7c54714ffd065db0c
sabita <sabitakhadka091@gmail.com>
2024-03-11 11:16:22 +0545

    created a fileinput which is imported from mantine/core

--- src/InputExample.jsx
+++ src/InputExample.jsx
@@ -1,5 +1,20 @@
+import { useState } from "react";
+import { FileInput } from "@mantine/core";
+
 function InputExample() {
-  return <div>InputExample</div>;
+  const [value, setValue] = useState("");
+
+  return (
+    <div>
+      <FileInput
+        label="Enter your name"
+        placeholder="John Doe"
+        value={value}
+        onChange={(event) => setValue(event.target.value)}
+      />
+      <p>Your name is: {value}</p>
+    </div>
+  );
 }
 
 export default InputExample;

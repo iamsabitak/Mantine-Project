13a526ee7826d6c117a85ce38d6dfc9d1e2b1ea6
sabita <sabitakhadka091@gmail.com>
2024-03-12 10:47:39 +0545

    removed the styled components and added mantine

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,83 +1,80 @@
-import { Button } from "@mantine/core";
-import styled from "styled-components";
-
-const ContactContainer = styled.div`
-  padding: 40px;
-`;
-
-const ContactHeading = styled.h2`
-  color: #333;
-  font-size: 24px;
-  margin-bottom: 20px;
-  text-align: center;
-`;
-
-const FormContainer = styled.div`
-  max-width: 400px;
-  margin: 0 auto;
-`;
-
-const FormGroup = styled.div`
-  margin-bottom: 20px;
-`;
-
-const Label = styled.label`
-  display: block;
-  color: #333;
-  margin-bottom: 5px;
-`;
-
-const Input = styled.input`
-  width: 100%;
-  padding: 10px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-`;
-
-const TextArea = styled.textarea`
-  width: 100%;
-  padding: 10px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-`;
-
-const SubmitButton = styled(Button)`
-  background-color: #007bff;
-  color: #fff;
-  padding: 10px 20px;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  transition: background-color 0.3s ease;
-  margin-left: 10rem;
-
-  &:hover {
-    background-color: #0056b3;
-  }
-`;
+import { Button, Input, TextInput, Title } from "@mantine/core";
 
 const Contact = () => {
   return (
-    <ContactContainer>
-      <ContactHeading>Contact Us</ContactHeading>
-      <FormContainer>
-        <form>
-          <FormGroup>
-            <Label htmlFor="name">Name</Label>
-            <Input type="text" id="name" />
-          </FormGroup>
-          <FormGroup>
-            <Label htmlFor="email">Email</Label>
-            <Input type="email" id="email" />
-          </FormGroup>
-          <FormGroup>
-            <Label htmlFor="message">Message</Label>
-            <TextArea id="message" rows="5"></TextArea>
-          </FormGroup>
-          <SubmitButton type="submit">Submit</SubmitButton>
-        </form>
-      </FormContainer>
-    </ContactContainer>
+    <>
+      <Title order={2} style={{ marginLeft: "35rem" }}>
+        Contact Us
+      </Title>
+      <Input.Wrapper style={{ marginLeft: "25rem" }}>
+        <TextInput
+          label="Name"
+          type="text"
+          placeholder="Full Name"
+          styles={{
+            input: {
+              border: "1px solid grey",
+              width: "50%",
+              height: "2rem",
+              fontSize: "1rem",
+              borderRadius: "5px",
+              marginTop: "15px",
+              marginBottom: "15px",
+            },
+            label: { fontSize: "1rem" },
+          }}
+        />
+        <TextInput
+          label="Email"
+          type="email"
+          placeholder="Email Address"
+          styles={{
+            input: {
+              border: "1px solid grey",
+              width: "50%",
+              height: "2rem",
+              fontSize: "1rem",
+              borderRadius: "5px",
+              marginTop: "15px",
+              marginBottom: "15px",
+            },
+            label: { fontSize: "1rem" },
+          }}
+        />
+        <TextInput
+          label="Message"
+          type="message"
+          placeholder="Message"
+          styles={{
+            input: {
+              border: "1px solid grey",
+              width: "50%",
+              height: "2rem",
+              fontSize: "1rem",
+              borderRadius: "5px",
+              marginTop: "15px",
+              marginBottom: "15px",
+            },
+            label: { fontSize: "1rem" },
+          }}
+        />
+        <Button
+          style={{
+            width: "5rem",
+            height: "2.3rem",
+            backgroundColor: "green",
+            border: "1px solid green",
+            boxShadow: "0 0 2px green",
+            borderRadius: "5px",
+            color: "white",
+            marginLeft: "10rem",
+            marginTop: "15px",
+          }}
+        >
+          Submit
+        </Button>
+      </Input.Wrapper>
+    </>
   );
 };
 

caa65193ef9706b395912c1e422de79652b9ee7a
sabita <sabitakhadka091@gmail.com>
2024-03-12 16:17:56 +0545

    created a multiselect functionality using mantine component

--- src/multiselect/MultiSelectComp.jsx
+++ src/multiselect/MultiSelectComp.jsx
@@ -1,10 +1,26 @@
-import { Title } from "@mantine/core";
+import { useState } from "react";
+import { MultiSelect } from "@mantine/core";
+
+import "@mantine/core/styles/global.css";
+import "@mantine/core/styles/Input.css";
 
 function MultiSelectComp() {
+  const [value, setValue] = useState([]);
+
+  const options = ["Kathmandu", "Pokhara", "Biratnagar", "Birgunj"];
+
   return (
-    <div>
-      <Title>MultiSelectComp</Title>
-    </div>
+    <>
+      <MultiSelect
+        label="Your favorite City"
+        placeholder="Pick value"
+        data={options}
+        value={value}
+        searchable
+        nothingFoundMessage="Nothing found..."
+        onChange={setValue}
+      />
+    </>
   );
 }
 

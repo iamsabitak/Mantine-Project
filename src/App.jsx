7ef84bca68e939a89bbc378802755f67d85eaa65
sabita <sabitakhadka091@gmail.com>
2024-03-12 16:37:15 +0545

    implemented appcomponent to the app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { MantineProvider } from "@mantine/core";
 
-// import AppContent from "./AppContent";
+import AppContent from "./AppContent";
 
 import { BrowserRouter } from "react-router-dom";
-import MultiSelectComp from "./multiselect/MultiSelectComp";
+// import MultiSelectComp from "./multiselect/MultiSelectComp";
 
 function App() {
   return (
     <>
       <MantineProvider>
-        <BrowserRouter>{/* <AppContent /> */}</BrowserRouter>
-        <MultiSelectComp />
+        <BrowserRouter>
+          <AppContent />
+        </BrowserRouter>
+        {/* <MultiSelectComp /> */}
       </MantineProvider>
     </>
   );

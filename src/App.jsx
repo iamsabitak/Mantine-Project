aa83db297265342a445820fed8cc724ec196bf55
sabita <sabitakhadka091@gmail.com>
2024-03-12 14:18:11 +0545

    chores:removed the unnecessary import from app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { MantineProvider } from "@mantine/core";
-// import InputExample from "./InputExample";
+
 import AppContent from "./AppContent";
+
 import { BrowserRouter } from "react-router-dom";
 
 function App() {

bdc8fbd6097a16e0277edd9aaf3909f54b603eb5
sabita <sabitakhadka091@gmail.com>
2024-03-12 11:47:57 +0545

    removed all styled component

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,105 +1,54 @@
-import { Button } from "@mantine/core";
-import styled from "styled-components";
-
-const ServicesContainer = styled.div`
-  padding: 40px;
-`;
-
-const Heading = styled.h2`
-  color: #333;
-  font-size: 24px;
-  text-align: center;
-  margin-bottom: 50px;
-`;
-
-const ServiceCard = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-bottom: 30px;
-`;
-
-const CardImage = styled.img`
-  max-height: 300px;
-`;
-
-const CardTitle = styled.h5`
-  color: #333;
-  font-size: 20px;
-  margin-top: 20px;
-`;
-
-const CardText = styled.p`
-  color: #555;
-  text-align: center;
-  margin-top: 10px;
-`;
-
-const LearnMoreButton = styled(Button)`
-  background-color: #007bff;
-  color: #fff;
-  padding: 10px 20px;
-  border: none;
-  border-radius: 5px;
-  text-decoration: none;
-  margin-top: 20px;
-  cursor: pointer;
-  transition: background-color 0.3s ease;
-
-  &:hover {
-    background-color: #0056b3;
-  }
-`;
+import { Button, Card, Image, Title } from "@mantine/core";
 
 const Services = () => {
   return (
-    <ServicesContainer>
-      <Heading>Our Services</Heading>
-      <div>
-        <ServiceCard className="col-md-4">
-          <CardImage
+    <>
+      <Title>Our Services</Title>
+      <div style={{ gridRow: "1" }}>
+        <>
+          <Card
             src="https://p7.hiclipart.com/preview/337/513/284/password-manager-privacy-policy-password-strength-random-password-generator-service.jpg"
             alt="Service 1"
           />
           <div>
-            <CardTitle>Service 1</CardTitle>
-            <CardText>
+            <Title>Service 1</Title>
+            <>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
               eget enim in lectus convallis tempus.
-            </CardText>
-            <LearnMoreButton>Learn More</LearnMoreButton>
+            </>
+            <Button variant="primary">Learn More</Button>
           </div>
-        </ServiceCard>
-        <ServiceCard className="col-md-4">
-          <CardImage
+        </>
+        <>
+          <Image
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKY9K_lb8olBs1-CHaGJx_DMOpJBuTc_7GIsKhgpToGSszLpnSfIdPG9CDS9SzgUcjW2E&usqp=CAU"
             alt="Service 2"
           />
           <div>
-            <CardTitle>Service 2</CardTitle>
-            <CardText>
+            <Title>Service 2</Title>
+            <>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
               eget enim in lectus convallis tempus.
-            </CardText>
-            <LearnMoreButton>Learn More</LearnMoreButton>
+            </>
+            <Button variant="primary">Learn More</Button>
           </div>
-        </ServiceCard>
-        <ServiceCard>
-          <CardImage
+        </>
+        <>
+          <Image
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYxggn3n_VnHanqYquI4yc5fQe9TclfT5-QCLq60ZNLS9q70YwtaWYkWtc04acYPBPv7s&usqp=CAU"
             alt="Service 3"
           />
           <div>
-            <CardTitle>Service 3</CardTitle>
-            <CardText>
+            <>Service 3</>
+            <>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
               eget enim in lectus convallis tempus.
-            </CardText>
-            <LearnMoreButton>Learn More</LearnMoreButton>
+            </>
+            <Button variant="primary">Learn More</Button>
           </div>
-        </ServiceCard>
+        </>
       </div>
-    </ServicesContainer>
+    </>
   );
 };
 

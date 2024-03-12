a423bd08b011f52ca8a5d91efe2538f021ad9001
sabita <sabitakhadka091@gmail.com>
2024-03-12 11:15:55 +0545

    remopved the styled component and added mantine

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,64 +1,30 @@
-import styled from "styled-components";
-
-const HomeContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 10px;
-  background-color: #f9f9f9;
-  border-radius: 10px;
-  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.1);
-`;
-
-const ContentContainer = styled.div`
-  flex: 1;
-`;
-
-const Heading = styled.h2`
-  color: #333;
-  font-size: 24px;
-  margin-bottom: 20px;
-`;
-
-const Paragraph = styled.p`
-  color: #555;
-  line-height: 1.6;
-`;
-
-const Image = styled.img`
-  max-width: 100%;
-  border-radius: 20px;
-`;
-const DefaultParagraph = styled.div`
-  margin-bottom: 10rem;
-`;
+import { Container, Group, Image, Text, Title } from "@mantine/core";
 
 const Home = () => {
   return (
-    <HomeContainer>
-      <ContentContainer>
-        <DefaultParagraph>
-          <Heading>Welcome to Our Website</Heading>
-          <Paragraph>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut arcu
-            nisi. Curabitur ac leo eu libero elementum convallis nec eget velit.
-            Proin consequat eros vel felis mattis, nec consectetur mauris
-            finibus.
-          </Paragraph>
-          <Paragraph>
-            Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
-            posuere cubilia curae; In aliquet turpis a urna maximus, nec
-            elementum velit luctus. Maecenas nec nunc mi.
-          </Paragraph>
-        </DefaultParagraph>
-      </ContentContainer>
-      <div>
-        <Image
-          src="https://media.istockphoto.com/id/1312128591/vector/home-icon-vector-house-sign.jpg?s=612x612&w=0&k=20&c=jxUT546g8Cfgv1SiAfh2Eos46XI3DqYvPLhxGFaobpE="
-          alt="Welcome Image"
-        />
-      </div>
-    </HomeContainer>
+    <Container style={{ display: "flex", marginTop: "-4rem" }}>
+      <Group style={{ marginTop: "10rem" }}>
+        <Title style={{ marginBottom: "20px", fontSize: "24px" }}>
+          Welcome to Our Website
+        </Title>
+        <Text style={{ lineHeight: "1.6" }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut arcu
+          nisi. Curabitur ac leo eu libero elementum convallis nec eget velit.
+          Proin consequat eros vel felis mattis, nec consectetur mauris finibus.
+        </Text>
+        <Text style={{ lineHeight: "1.6" }}>
+          Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+          posuere cubilia curae; In aliquet turpis a urna maximus, nec elementum
+          velit luctus. Maecenas nec nunc mi.
+        </Text>
+      </Group>
+
+      <Image
+        src="https://media.istockphoto.com/id/1312128591/vector/home-icon-vector-house-sign.jpg?s=612x612&w=0&k=20&c=jxUT546g8Cfgv1SiAfh2Eos46XI3DqYvPLhxGFaobpE="
+        alt="Welcome Image"
+        style={{ zIndex: "-1" }}
+      />
+    </Container>
   );
 };
 

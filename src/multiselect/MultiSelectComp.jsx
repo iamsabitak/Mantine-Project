db716a8f7946ded9f0613a8026cc1f50c984155e
sabita <sabitakhadka091@gmail.com>
2024-03-13 11:45:12 +0545

    styled the multiselectcomp by importing mantine style css file list

--- src/multiselect/MultiSelectComp.jsx
+++ src/multiselect/MultiSelectComp.jsx
@@ -1,8 +1,14 @@
 import { useState } from "react";
+
 import { MultiSelect } from "@mantine/core";
 
-import "@mantine/core/styles/global.css";
-import "@mantine/core/styles/Input.css";
+import "@mantine/core/styles/global.css"; //for global style
+import "@mantine/core/styles/Combobox.css"; // style the big tick marks
+import "@mantine/core/styles/Input.css"; //for input style
+import "@mantine/core/styles/Pill.css"; //to make button type those which are added
+import "@mantine/core/styles/PillsInput.css"; //to remove inner input from the input
+import "@mantine/core/styles/Popover.css"; //to style the options
+import "@mantine/core/styles/UnstyledButton.css"; // to style the UnstyledButton {cross marks}
 
 function MultiSelectComp() {
   const [value, setValue] = useState([]);
@@ -19,6 +25,14 @@ function MultiSelectComp() {
         searchable
         nothingFoundMessage="Nothing found..."
         onChange={setValue}
+        styles={{
+          label: {
+            marginLeft: "33rem",
+            marginBottom: "1rem",
+            marginTop: "1rem",
+            fontSize: "18px",
+          },
+        }}
       />
     </>
   );

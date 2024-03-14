2d657bedd2847ff8cd81d217441740767d40186a
sabita <sabitakhadka091@gmail.com>
2024-03-14 13:52:08 +0545

    removed the mantine css file import and added inline css

--- src/multiselect/MultiSelectComp.jsx
+++ src/multiselect/MultiSelectComp.jsx
@@ -2,13 +2,6 @@ import { useState } from "react";
 
 import { MultiSelect } from "@mantine/core";
 
-import "@mantine/core/styles/global.css"; // for global style
-import "@mantine/core/styles/Combobox.css"; // style the big tick marks
-import "@mantine/core/styles/Input.css"; // for input style
-
-import "@mantine/core/styles/PillsInput.css"; // to remove inner input from the input
-import "@mantine/core/styles/Popover.css"; // to style the options
-
 import PaperChipComp from "./PaperChipComp";
 
 function MultiSelectComp() {
@@ -42,7 +35,9 @@ function MultiSelectComp() {
         alignItems: "flex-start",
       }}
     >
-      <div style={{ marginLeft: "3rem", maxWidth: "300px" }}>
+      <div
+        style={{ marginLeft: "3rem", maxWidth: "300px", marginRight: "5rem" }}
+      >
         <MultiSelect
           label="Your favorite Country"
           placeholder="Pick value"
@@ -55,12 +50,27 @@ function MultiSelectComp() {
           hidePickedOptions
           styles={{
             label: {
-              marginLeft: "1rem",
+              marginLeft: "6px",
               marginBottom: "1rem",
               marginTop: "1rem",
               fontSize: "15px",
             },
             pill: { display: "none" },
+            section: { display: "none" },
+            dropdown: {
+              border: "1px solid #A8A8A8",
+              borderRadius: "5px",
+              marginLeft: "2.8rem",
+            },
+            options: {
+              margin: "1rem",
+              marginTop: "4px",
+              cursor: "pointer",
+            },
+            option: {
+              marginTop: "10px",
+              // borderTop: "1px solid #A8A8A8",
+            },
           }}
         />
       </div>
